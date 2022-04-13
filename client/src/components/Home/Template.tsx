94a2ffe0315f40dd32e506aa39a3108dfe4ef3e8
snovamicron <mdip5675@gmail.com>
2022-04-13 12:52:43 +0530

    commit 4

--- client/src/components/Home/Template.tsx
+++ client/src/components/Home/Template.tsx
@@ -1,77 +1,28 @@
 
 
 // components
-import Navbar from "./Template/Navbar"
-// import Template_1 from "./Template/Template_1"
+import Navbar from './Template/Navbar'
+import Template_1 from './Template/Template_1'
 
 // MUI components
-import { Box, useMediaQuery } from '@mui/material'
-import { makeStyles } from "@mui/styles"
-import { useTheme } from "@mui/material/styles"
-
-
+import { Box } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 
 
 const Template = () => {
-  const theme = useTheme()
-  const underMedium = useMediaQuery(theme.breakpoints.down('md'))
-  const upperMedium = useMediaQuery(theme.breakpoints.up('md'))
-  interface componentsMediaQueryInterface {
-    formBoxWidth: string,
-    formBoxHeight: string,
-    wraperflexDirection: string
+const useStyles = makeStyles({
+  wraper:{
+    border:'2px solid #000',
+      height:'92vh',
+      marginTop:64
   }
-  const componentsMediaQuery = (): componentsMediaQueryInterface => {
-    switch (true) {
-      case underMedium:
-        return {
-          formBoxWidth: '100%',
-          formBoxHeight: '100%',
-          wraperflexDirection: 'column'
-
-
-        }
-      case upperMedium:
-        return {
-          formBoxWidth: '50%',
-          formBoxHeight: '90%',
-          wraperflexDirection: 'row'
-        }
-      default:
-        return {
-          formBoxWidth: '50%',
-          formBoxHeight: '90%',
-          wraperflexDirection: 'row'
-        }
-    }
-  }
-
-  const useStyles = makeStyles({
-    wraper: {
-      marginTop: 68,
-      height: '91vh',
-      width: '100vw',
-      display: 'flex',
-      flexDirection:'column',
-      alignItems: 'center',
-    },
-    formBox: {
-      border: '2px solid #000',
-      height: componentsMediaQuery().formBoxHeight,
-      width: componentsMediaQuery().formBoxWidth
-    }
-  })
-  const classes = useStyles()
+})
+const classes = useStyles()
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <Box className={classes.wraper}>
-        <Box className={classes.formBox}>
-
-        </Box>
-        <Box className={classes.formBox}>
-
-        </Box>
+        <Template_1/>
       </Box>
     </>
   )
--- client/src/components/Home/Template/Template_1.tsx
+++ client/src/components/Home/Template/Template_1.tsx
@@ -1,23 +1,147 @@
+import { useState } from 'react'
 
 // MUI components
-import { Box, TextField } from '@mui/material'
+import {
+    Box,
+    Grid,
+    useMediaQuery,
+    TextField,
+    TextareaAutosize,
+    Button
+} from '@mui/material'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useTheme } from '@mui/material/styles'
 import { makeStyles } from '@mui/styles'
 
-const Template_1 = ()=>{
-    const useStyles = makeStyles({
-        wraper:{
 
+const Template_1 = () => {
+    interface porjectInterface {
+        porjectName: string[],
+        porjectDescription: string
+    }
+    interface formDataInterface {
+        name: string,
+        bio: string,
+        experience: string,
+        education: string,
+        skills: string[],
+        languages: string[],
+        contacts: string[],
+        projects: porjectInterface[]
+    }
+    const [formData, setFormData] = useState<formDataInterface>({
+        name: '',
+        bio: '',
+        experience: '',
+        education: '',
+        skills: [],
+        languages: [],
+        contacts: [],
+        projects: []
+    })
+    const onDataChange = (e: any): void => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+    const theme = useTheme()
+    const matches = useMediaQuery(theme.breakpoints.up('md'))
+    const useStyles = makeStyles({
+        components: {
+            border: '2px solid #000',
+            height: '100%',
+            padding: 10,
+            boxSizing: 'border-box'
         }
     })
     const classes = useStyles()
-    return(
+    return (
         <>
-        <Box className={classes.wraper}>
-            <TextField/>
-            <TextField/>
-            <TextField/>
-            <TextField/>
-        </Box>
+            <Grid container sx={{ height: '100%', padding: '10px 10px' }} columnSpacing={{ xs: 1 }} >
+                <Grid item xs={12} md={7} >
+                    <Box className={classes.components}>
+                        <Grid container spacing={{ xs: 2 }}>
+                            <Grid item xs={12} lg={7}>
+                                <TextField
+                                    fullWidth
+                                    label='name'
+                                    value={formData.name}
+                                    name='name'
+                                    onChange={(e) => onDataChange(e)}
+                                />
+                            </Grid>
+                            <Grid item xs={12} lg={5}>
+                                <TextareaAutosize
+                                    placeholder='bio'
+                                    style={{ width: '100%' }}
+                                    minRows={3}
+                                    value={formData.bio}
+                                    name='bio'
+                                    onChange={(e) => onDataChange(e)}
+                                />
+                            </Grid>
+                            <Grid item xs={12} lg={6}>
+                                <TextareaAutosize
+                                    placeholder='education'
+                                    style={{ width: '100%' }}
+                                    minRows={3}
+                                    value={formData.education}
+                                    name='education'
+                                    onChange={(e) => onDataChange(e)}
+                                />
+                            </Grid>
+                            <Grid item xs={12} lg={6}>
+                                <TextareaAutosize
+                                    placeholder='experience'
+                                    style={{ width: '100%' }}
+                                    minRows={3}
+                                    value={formData.experience}
+                                    name='experience'
+                                    onChange={(e) => onDataChange(e)}
+                                />
+                            </Grid>
+                            <Grid item xs={12} lg={4}>
+                                <Box>
+                                    <Button
+                                        variant='outlined'
+                                        startIcon={<AddCircleOutlineIcon />}
+                                        size='medium'
+                                    >
+                                        add skills
+                                    </Button>
+
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12} lg={4}>
+                                <Box>
+                                    <Button
+                                        variant='outlined'
+                                        startIcon={<AddCircleOutlineIcon />}
+                                        size='medium'
+                                    >
+                                        add languages
+                                    </Button>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12} lg={4}>
+                                <Box>
+                                    <Button
+                                        variant='outlined'
+                                        startIcon={<AddCircleOutlineIcon />}
+                                        size='medium'
+                                    >
+                                        add contacts
+                                    </Button>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Grid>
+                {
+                    matches &&
+                    <Grid item xs={12} md={5}>
+                        <Box className={classes.components}>prevew</Box>
+                    </Grid>
+                }
+            </Grid>
         </>
     )
 }
