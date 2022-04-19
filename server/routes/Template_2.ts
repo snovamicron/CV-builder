04782c6426f82a78411aa30bfebef99210b18095
snovamicron <mdip5675@gmail.com>
2022-04-19 22:20:09 +0530

    commit 11

---
+++ server/routes/Template_2.ts
@@ -0,0 +1,180 @@
+import express from 'express'
+import PDFDocument from 'pdfkit'
+import { temp_2Obj } from '../utils/templateObj'
+
+const router = express.Router()
+
+var temp_1 = new temp_2Obj(
+    '','','','','',
+    '','','','','',
+    '','','','','',
+    '','',[]
+)
+router.post('/temp_2_data',(req, res)=>{
+    const {
+        name,
+        address,
+        contact_number,
+        email,
+        Career_Objective,
+        Educational_Qualification,
+        Work_Experience,
+        dob,
+        Father_Name,
+        sex,
+        Marital_Status,
+        Languages_Known,
+        Technical_Skills,
+        Nationality,
+        Hobbies_Interest,
+        Declaration,
+        place,
+        date,
+    } = req.body
+    temp_1 = new temp_2Obj(
+        name,
+        address,
+        contact_number,
+        email,
+        Career_Objective,
+        Educational_Qualification,
+        Work_Experience,
+        dob,
+        Father_Name,
+        sex,
+        Marital_Status,
+        Languages_Known,
+        Technical_Skills,
+        Nationality,
+        Hobbies_Interest,
+        Declaration,
+        place,
+        date,
+    )
+    res.status(200).send('CV is ready')
+  })
+router.get('/template_2',(req, res)=>{
+    const doc = new PDFDocument({
+        size:'A4',
+        margins:{
+            top:60,
+            bottom:50,
+            left:75,
+            right:50
+        }
+    })
+    doc.lineGap(1.5)
+    doc.font('Times-Bold')
+    doc.fontSize(15)
+    doc.text(temp_1.name, {
+    })
+    doc.font('Times-Roman')
+    doc.fontSize(12)
+    doc.text(temp_1.address)
+    doc.text(`Mobile no: ${temp_1.contact_number}`)
+    doc.text(`Email id: ${temp_1.email}`)
+    doc.moveTo(65, 130)
+        .lineTo(570, 130)
+        .lineWidth(2.5)
+        .stroke()
+    doc.moveDown(2)
+    doc.font('Times-Bold')
+    doc.text(`Career Objective`, {
+        underline: true
+    })
+    doc.moveDown(0.5)
+    doc.font('Times-Roman')
+    doc.text(`${temp_1.Career_Objective}`,{
+        indent:35
+    })
+    doc.moveDown(1.5)
+    doc.font('Times-Bold')
+    doc.text(`Educational Qualification`, {
+        underline: true
+    })
+    doc.moveDown(0.3)
+    doc.font('Times-Roman')
+    doc.text(`${temp_1.Educational_Qualification}`,{
+        indent:35
+    })
+    doc.moveDown(3)
+    doc.font('Times-Bold')
+    doc.text(`Work Experience`, {
+        underline: true
+    })
+    doc.moveDown(0.3)
+    doc.font('Times-Roman')
+    doc.text(`${temp_1.Work_Experience}`,{
+        indent:35
+    })
+    doc.moveDown(3)
+    doc.font('Times-Bold')
+    doc.text(`Technical Skills`, {
+        underline: true
+    })
+    doc.moveDown(0.5)
+    doc.font('Times-Roman')
+    doc.list(temp_1.Technical_Skills,{
+        indent:35
+    })
+    doc.moveDown(2.5)
+    doc.font('Times-Bold')
+    doc.text(`Personal Details`, {
+        underline: true
+    })
+    doc.moveDown(1.5)
+    doc.font('Times-Roman')
+    doc.text('Name                           : '+ temp_1.name,{
+        indent:35
+    })
+    doc.moveDown(0.3)
+    doc.text('Date of Birth               : '+ temp_1.dob,{
+        indent:35
+    })
+    doc.moveDown(0.3)
+    doc.text('Father Name               : '+temp_1.Father_Name,{
+        indent:35
+    })
+    doc.moveDown(0.3)
+    doc.text("Sex                              : "+temp_1.sex,{
+        indent:35
+    })
+    doc.moveDown(0.3)
+    doc.text(`Marital Status              : ${temp_1.Marital_Status}`,{
+        indent:35
+    })
+    doc.moveDown(0.3)
+    doc.text(`Languages Known      : ${temp_1.Languages_Known}`,{
+        indent:35   
+    })
+    doc.moveDown(0.3)
+    doc.text(`Nationality                  : ${temp_1.Nationality}`,{
+        indent:35
+    })
+    doc.moveDown(0.3)
+    doc.text(`Hobbies & Interest      : ${temp_1.Hobbies_Interest}`,{
+        indent:35
+    })
+    doc.moveDown(3)
+    doc.font('Times-Bold')
+    doc.text(`Declaration`, {
+        underline: true
+    })
+    doc.moveDown(0.8)
+    doc.font('Times-Roman')
+    doc.text(temp_1.Declaration,{
+        indent:35
+    })
+    doc.moveDown(1.5)
+    doc.text(`(${temp_1.name})`,{
+        indent:320
+    })
+    doc.moveUp()
+    doc.text(`Place: ${temp_1.place}`)
+    doc.text(`Date: ${temp_1.date}`)
+    doc.end()
+    doc.pipe(res)
+})
+
+
+export default router
--- server/utils/templateObj.ts
+++ server/utils/templateObj.ts
@@ -5,7 +5,7 @@ export type porjectInterface = {
 
 export class temp_1Obj {
     constructor(
-        name:string,
+    name:string,
     bio:string,
     experience:string,
     skills:string[],
@@ -31,6 +31,67 @@ export class temp_1Obj {
     contacts:string[];
     education:string;
     projects:porjectInterface[]
+}
+
+
+export class temp_2Obj {
+    constructor(
+    name:string,
+    address:string,
+    contact_number:string,
+    email:string,
+    Career_Objective:string,
+    Educational_Qualification:string,
+    Work_Experience:string,
+    dob:string,
+    Father_Name:string,
+    sex:string,
+    Marital_Status:string,
+    Languages_Known:string,
+    Nationality:string,
+    Hobbies_Interest:string,
+    Declaration:string,
+    place:string,
+    date:string,
+    Technical_Skills:string[]
+    ){
+        this.name = name
+        this.address = address
+        this.contact_number = contact_number
+        this.email = email
+        this.Career_Objective = Career_Objective
+        this.Educational_Qualification = Educational_Qualification
+        this.Work_Experience = Work_Experience
+        this.dob = dob, 
+        this.Father_Name = Father_Name, 
+        this.sex = sex, 
+        this.Marital_Status = Marital_Status, 
+        this.Languages_Known = Languages_Known, 
+        this.Technical_Skills = Technical_Skills, 
+        this.Nationality = Nationality, 
+        this.Hobbies_Interest = Hobbies_Interest, 
+        this.Declaration = Declaration,
+        this.place = place,
+        this.date = date
+    }
+    name:string;
+    address:string;
+    contact_number:string;
+    email:string;
+    Career_Objective:string;
+    Educational_Qualification:string;
+    Work_Experience:string;
+    dob:string;
+    Father_Name:string;
+    sex:string;
+    Marital_Status:string;
+    Languages_Known:string;
+    Technical_Skills:string[];
+    Nationality:string;
+    Hobbies_Interest:string;
+    Declaration:string;
+    place:string;
+    date:string;
 }
 
 
